054113772e30d04a69f7cff67ee3f583716557cd
Yichao Yu <yyc1992@gmail.com>
2026-03-28 22:50:27 -0400

    Implement ready buffer for AXI protocol handling

--- amaranth_axi/axitools.py
+++ amaranth_axi/axitools.py
@@ -1,6 +1,47 @@
 #
 
 from amaranth import *
+from transactron import TModule, Method, def_method
+
+
+class ReadyBuffer(Elaboratable):
+    def __init__(self, *, ready, valid, data, domain='sync'):
+        self._ready = ready
+        self._valid = valid
+        self._data = data
+        self.domain = domain
+        self.get = Method(o=[('data', data.shape())])
+        self.peek = Method(o=[('valid', 1), ('data', data.shape())])
+
+    def elaborate(self, plat):
+        m = TModule()
+
+        empty = Signal(1, init=1)
+
+        buf = Signal(self._data.shape())
+
+        m.d.comb += self._ready.eq(empty)
+
+        out_valid = ~empty | self._valid
+        out_data = Mux(empty, self._data, buf)
+
+        with m.If(empty & self._valid):
+            # If there's something coming in we'll assume it fills the cache.
+            # If there's a simultaneous read the `get` method will set the `empty` flag.
+            m.d[self.domain] += [empty.eq(0),
+                                 buf.eq(self._data)]
+
+        @def_method(m, self.get, ready=out_valid)
+        def _():
+            m.d[self.domain] += empty.eq(1)
+            return dict(data=out_data)
+
+        @def_method(m, self.peek, nonexclusive=True)
+        def _():
+            return dict(valid=out_valid, data=out_data)
+
+        return m
+
 
 def axi_write_reg(m, reg, data, strb, *, domain='sync'):
     width = len(data)
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -4,10 +4,14 @@ from amaranth import *
 from amaranth.lib import wiring
 from amaranth.lib.wiring import In, Out
 
-from transactron import TModule, Method, def_method
+from transactron import TModule, Transaction, Method, def_method, TransactronContextComponent
 from transactron.testing import TestCaseWithSimulator, SimpleTestCircuit
 
-from amaranth_axi.axitools import axi_write_reg
+from amaranth_axi.axitools import axi_write_reg, ReadyBuffer
+
+from .utils import synth
+
+import random
 
 class AXIRegister(wiring.Component):
     def __init__(self):
@@ -65,3 +69,149 @@ class TestAXIWriteReg(TestCaseWithSimulator):
 
         with self.run_simulation(circ) as sim:
             sim.add_testbench(setreg)
+
+
+class ReadyBufferWrapper(wiring.Component):
+    i_valid: In(1)
+    i_ready: Out(1)
+    i_data: In(32)
+
+    o_valid: Out(1)
+    o_ready: In(1)
+    o_data: Out(32)
+
+    def elaborate(self, plat):
+        m = TModule()
+
+        m.submodules.buff = buff = ReadyBuffer(ready=self.i_ready, valid=self.i_valid,
+                                               data=self.i_data)
+
+        with Transaction().body(m):
+            status = buff.peek(m)
+            m.d.top_comb += [self.o_valid.eq(status.valid),
+                             self.o_data.eq(status.data)]
+
+        with Transaction().body(m, ready=self.o_ready):
+            buff.get(m)
+
+        return m
+
+def test_synth_ready_buffer():
+    buff = TransactronContextComponent(ReadyBufferWrapper())
+    synth(buff, ports=[buff.i_valid, buff.i_ready, buff.i_data,
+                       buff.o_valid, buff.o_ready, buff.o_data])
+
+class TestReadyBuffer(TestCaseWithSimulator):
+    def test_init(self):
+        ready = Signal(1)
+        valid = Signal(1)
+        data = Signal(32)
+
+        buff = ReadyBuffer(ready=ready, valid=valid, data=data)
+        circ = SimpleTestCircuit(buff)
+
+        async def init(sim):
+            await sim.tick()
+            assert sim.get(ready) == 1
+
+        with self.run_simulation(circ) as sim:
+            sim.add_testbench(init)
+
+    def test_full_throughput(self):
+        buff = ReadyBufferWrapper()
+        circ = SimpleTestCircuit(buff)
+
+        cycles = 256
+
+        async def full(sim):
+            sim.set(buff.i_valid, 0)
+            assert sim.get(buff.o_valid) == 0
+            sim.set(buff.i_valid, 1)
+            assert sim.get(buff.o_valid) == 1
+            sim.set(buff.o_ready, 1)
+
+            for _ in range(cycles):
+                v = random.randint(0, 0xffff_ffff)
+                sim.set(buff.i_data, v)
+                await sim.tick()
+                assert sim.get(buff.o_data) == v
+                assert sim.get(buff.o_valid) == 1
+                sim.set(buff.i_valid, 0)
+                assert sim.get(buff.o_valid) == 0
+                sim.set(buff.i_valid, 1)
+                assert sim.get(buff.o_valid) == 1
+
+        with self.run_simulation(circ) as sim:
+            sim.add_testbench(full)
+
+    def test_buffer(self):
+        buff = ReadyBufferWrapper()
+        circ = SimpleTestCircuit(buff)
+
+        cycles = 256
+
+        async def buffer(sim):
+            sim.set(buff.i_valid, 0)
+            sim.set(buff.o_ready, 0)
+            sim.set(buff.i_data, 0)
+            assert sim.get(buff.i_ready) == 1
+            assert sim.get(buff.o_ready) == 0
+
+            i_v0 = 0
+            i_r0 = 1
+            i_d = 0
+            o_v0 = 0
+            o_r0 = 0
+            o_d = 0
+
+            data_in = []
+            data_out = []
+
+            for _ in range(cycles):
+                i_v = i_v0
+                if (not i_v0) or i_r0:
+                    i_v = random.randint(0, 1)
+                    sim.set(buff.i_valid, i_v)
+                    if i_v and (not i_v0 or i_r0):
+                        i_d = random.randint(0, 0xffff_ffff)
+                        sim.set(buff.i_data, i_d)
+                o_r = o_r0
+                if (not o_r0) or o_v0:
+                    o_r = random.randint(0, 1)
+                    sim.set(buff.o_ready, o_r)
+
+                in_transfer = i_v and sim.get(buff.i_ready)
+                out_transfer = o_r and sim.get(buff.i_valid)
+
+                if in_transfer:
+                    data_in.append(i_d)
+                if out_transfer:
+                    data_out.append(sim.get(buff.o_data))
+
+                await sim.tick()
+
+                i_r = int(o_r or (i_r0 and not i_v))
+                o_v = int(i_v or (o_v0 and not o_r0))
+                assert sim.get(buff.i_valid) == i_v
+                assert sim.get(buff.i_ready) == i_r
+                assert sim.get(buff.o_valid) == o_v
+                assert sim.get(buff.o_ready) == o_r
+
+                if i_r0:
+                    o_d = i_d
+                if o_v:
+                    assert sim.get(buff.o_data) == o_d
+
+                i_v0 = i_v
+                i_r0 = i_r
+                o_v0 = o_v
+                o_r0 = o_r
+
+            assert len(data_in) >= len(data_out)
+            if len(data_in) > len(data_out):
+                assert len(data_in) == len(data_out) + 1
+                data_in = data_in[:-1]
+            assert data_in == data_out
+
+        with self.run_simulation(circ) as sim:
+            sim.add_testbench(buffer)
