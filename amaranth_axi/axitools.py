74ce3b9935254a4905066d662f0e5086f59390b5
Yichao Yu <yyc1992@gmail.com>
2026-04-16 22:23:39 -0400

    AXI Lite master interface

--- amaranth_axi/axitools.py
+++ amaranth_axi/axitools.py
@@ -100,6 +100,88 @@ class AXILSlaveReadIFace(Elaboratable):
         return m
 
 
+class AXILMasterWriteIFace(Elaboratable):
+    def __init__(self, axil, *, domain='sync', align_address=True, **kws):
+        self._axil = axil
+        self._data_width = len(axil.WDATA)
+        self.domain = domain
+        self._in_buffered, self._out_buffered = _parse_buffered(**kws)
+        self._clear_bits = exact_log2(self._data_width//8) if align_address else 0
+        self._request = Method(i=[('addr', len(axil.AWADDR)), ('data', self._data_width),
+                                  ('strb', self._data_width//8)])
+        self.reply = Method(o=[('resp', 2)])
+
+    def request(self, m, /, addr, data, strb=None):
+        nbytes = self._data_width // 8
+        if strb is None:
+            strb = (1 << nbytes) - 1
+        return self._request(m, addr=addr, data=data, strb=strb)
+
+    def elaborate(self, plat):
+        m = TModule()
+
+        axil = self._axil
+        addr_width = len(axil.AWADDR)
+
+        m.submodules.wa_adapt = wa_adapt = OutAdaptor.from_signal(
+            ready=axil.AWREADY, valid=axil.AWVALID,
+            data=axil.AWADDR[self._clear_bits:], buffered=self._out_buffered)
+
+        m.submodules.wd_adapt = wd_adapt = OutAdaptor.from_signal(
+            ready=axil.WREADY, valid=axil.WVALID,
+            data=Cat(axil.WDATA, axil.WSTRB), buffered=self._out_buffered)
+
+        @def_method(m, self._request)
+        def _(addr, data, strb):
+            wa_adapt.output(m, addr[self._clear_bits:])
+            wd_adapt.output(m, Cat(data, strb))
+
+        m.submodules.b_adapt = b_adapt = InAdaptor.from_signal(
+            ready=axil.BREADY, valid=axil.BVALID,
+            data=axil.BRESP, buffered=self._in_buffered)
+
+        @def_method(m, self.reply)
+        def _():
+            return b_adapt.input(m).DATA
+
+        return m
+
+
+class AXILMasterReadIFace(Elaboratable):
+    def __init__(self, axil, *, domain='sync', align_address=True, **kws):
+        self._axil = axil
+        self._data_width = len(axil.RDATA)
+        self.domain = domain
+        self._in_buffered, self._out_buffered = _parse_buffered(**kws)
+        self._clear_bits = exact_log2(self._data_width//8) if align_address else 0
+        self.request = Method(i=[('addr', len(axil.ARADDR))])
+        self.reply = Method(o=[('data', self._data_width), ('resp', 2)])
+
+    def elaborate(self, plat):
+        m = TModule()
+
+        axil = self._axil
+
+        m.submodules.ra_adapt = ra_adapt = OutAdaptor.from_signal(
+            ready=axil.ARREADY, valid=axil.ARVALID,
+            data=axil.ARADDR[self._clear_bits:], buffered=self._out_buffered)
+
+        @def_method(m, self.request)
+        def _(addr):
+            ra_adapt.output(m, addr[self._clear_bits:])
+
+        m.submodules.rd_adapt = rd_adapt = InAdaptor.from_signal(
+            ready=axil.RREADY, valid=axil.RVALID,
+            data=Cat(axil.RDATA, axil.RRESP), buffered=self._in_buffered)
+
+        @def_method(m, self.reply)
+        def _():
+            d = rd_adapt.input(m).DATA
+            return dict(data=d[:self._data_width], resp=d[self._data_width:])
+
+        return m
+
+
 def axi_write_reg(m, reg, data, strb, *, domain='sync'):
     width = len(data)
     for i in range(width//8):
